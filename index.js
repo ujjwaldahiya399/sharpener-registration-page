f42ba36f497e53899382406d30324c5af684e218
Ujjwal dahiya <ujjwaldahiya350@gmail.com>
2023-08-04 10:49:47 +0530

    second

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 let nameInput = document.getElementById("name")
 var namee = ""
 nameInput.addEventListener("keypress", function(e) {
@@ -18,8 +17,29 @@ form.onsubmit = function(e) {
     console.log(namee, email);
     console.log("submit");
     let name = document.getElementById("name").value;
-    let url = " https://crudcrud.com/api/7b7bdbd59803493782d72b27bf895efc/post";
-    axios.post(url,{
-
-    })
+    let url = "https://crudcrud.com/api/e00755a28ff54a07aae0f7c4aa7d771d/posts";
+    if (namee !=="" && email !== "") {
+        axios.post(url,
+            {
+                "name":namee,
+                "email":email
+            }
+        )
+        showData();
+    } else {
+        alert("Please enter a name and email");
+    }
+    
+}
+let showData = async() =>{
+    let url = "https://crudcrud.com/api/e00755a28ff54a07aae0f7c4aa7d771d/posts";
+    const someData = await axios.get(url).then((res) => res.data);
+    console.log(someData);
+    let ul = document.querySelector("ul");
+    ul.innerHTML = ""
+    someData.forEach(element => {
+        let li = document.createElement("li");
+        li.textContent = `${element.name} - ${element.email}`;
+        ul.appendChild(li);
+    });
 }
